570f7ca2aacd6c505a34c77a1f000dda7c6834a0
Bruno S Marques <brunosmarques1995@hotmail.com>
2023-10-11 09:13:37 -0300

    Added basic cmake project

---
+++ VulkanSample/VulkanSample.cpp
@@ -0,0 +1,12 @@
+﻿// VulkanSample.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "VulkanSample.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
